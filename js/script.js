ab4b3105d18acfa521cf5c687aae7500eba1ebf9
misalazeem <55094882+misalazeem@users.noreply.github.com>
2023-04-10 18:11:39 +0500

    fixing linter errors

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
 
-	document.querySelector('#hamburger').addEventListener('click', function() {        
+	document.querySelector('#hamburger').addEventListener('click', () => {        
 		document.querySelector('#mobilemenu').classList.remove('mobile-menu-inactive');
         document.querySelector('#mobilemenu').classList.add('mobile-menu');
 	});
 
-    document.querySelector('.disabled-button').addEventListener('click', function() {
+    document.querySelector('.disabled-button').addEventListener('click', () => {
 		document.querySelector('#mobilemenu').classList.add('mobile-menu-inactive');
         document.querySelector('#mobilemenu').classList.remove('mobile-menu');
 	});
 
-    document.querySelector('#button1').addEventListener('click', function() {
+    document.querySelector('#button1').addEventListener('click', () => {
 		document.querySelector('#mobilemenu').classList.add('mobile-menu-inactive');
         document.querySelector('#mobilemenu').classList.remove('mobile-menu');
 	});
 
-    document.querySelector('#button2').addEventListener('click', function() {
+    document.querySelector('#button2').addEventListener('click', () => {
 		document.querySelector('#mobilemenu').classList.add('mobile-menu-inactive');
         document.querySelector('#mobilemenu').classList.remove('mobile-menu');
 	});
 
-    document.querySelector('#button3').addEventListener('click', function() {
+    document.querySelector('#button3').addEventListener('click', () => {
 		document.querySelector('#mobilemenu').classList.add('mobile-menu-inactive');
         document.querySelector('#mobilemenu').classList.remove('mobile-menu');
 	});
