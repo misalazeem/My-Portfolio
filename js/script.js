7437ba77c58c83c0111136d1699721b5380f65ad
Denio Nhanale <dnhanale@gmail.com>
2023-04-10 14:32:56 +0200

    Add js file

---
+++ js/script.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+	document.querySelector('#hamburger').addEventListener("click", function() {        
+		document.querySelector("#mobilemenu").classList.remove("mobile-menu-inactive");
+        document.querySelector("#mobilemenu").classList.add("mobile-menu");
+	});
+
+    document.querySelector('.disabled-button').addEventListener("click", function() {
+		document.querySelector("#mobilemenu").classList.add("mobile-menu-inactive");
+        document.querySelector("#mobilemenu").classList.remove("mobile-menu");
+	});
+
+    document.querySelector('#button1').addEventListener("click", function() {
+		document.querySelector("#mobilemenu").classList.add("mobile-menu-inactive");
+        document.querySelector("#mobilemenu").classList.remove("mobile-menu");
+	});
+
+    document.querySelector('#button2').addEventListener("click", function() {
+		document.querySelector("#mobilemenu").classList.add("mobile-menu-inactive");
+        document.querySelector("#mobilemenu").classList.remove("mobile-menu");
+	});
+
+    document.querySelector('#button3').addEventListener("click", function() {
+		document.querySelector("#mobilemenu").classList.add("mobile-menu-inactive");
+        document.querySelector("#mobilemenu").classList.remove("mobile-menu");
+	});
+	
+});
