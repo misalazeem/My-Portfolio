1f498b48a6520a26aeada36e0ca0ffb0113ae415
misalazeem <55094882+misalazeem@users.noreply.github.com>
2023-04-10 18:18:35 +0500

    fixing linter errors

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  document.querySelector('#hamburger').addEventListener('click', () => {        
-	document.querySelector('#mobilemenu').classList.remove('mobile-menu-inactive');
-	document.querySelector('#mobilemenu').classList.add('mobile-menu');
-  });
+document.querySelector('#hamburger').addEventListener('click', () => {        
+  document.querySelector('#mobilemenu').classList.remove('mobile-menu-inactive');
+  document.querySelector('#mobilemenu').classList.add('mobile-menu');
+});
 
-  document.querySelector('.disabled-button').addEventListener('click', () => {
-	document.querySelector('#mobilemenu').classList.add('mobile-menu-inactive');
-	document.querySelector('#mobilemenu').classList.remove('mobile-menu');
-  });
+document.querySelector('.disabled-button').addEventListener('click', () => {
+  document.querySelector('#mobilemenu').classList.add('mobile-menu-inactive');
+  document.querySelector('#mobilemenu').classList.remove('mobile-menu');
+});
 
-  document.querySelector('#button1').addEventListener('click', () => {
-	document.querySelector('#mobilemenu').classList.add('mobile-menu-inactive');
-	document.querySelector('#mobilemenu').classList.remove('mobile-menu');
-  });
+document.querySelector('#button1').addEventListener('click', () => {
+  document.querySelector('#mobilemenu').classList.add('mobile-menu-inactive');
+  document.querySelector('#mobilemenu').classList.remove('mobile-menu');
+});
 
-  document.querySelector('#button2').addEventListener('click', () => {
-	document.querySelector('#mobilemenu').classList.add('mobile-menu-inactive');
-	document.querySelector('#mobilemenu').classList.remove('mobile-menu');
-  });
+document.querySelector('#button2').addEventListener('click', () => {
+  document.querySelector('#mobilemenu').classList.add('mobile-menu-inactive');
+  document.querySelector('#mobilemenu').classList.remove('mobile-menu');
+});
 
-  document.querySelector('#button3').addEventListener('click', () => {
-	document.querySelector('#mobilemenu').classList.add('mobile-menu-inactive');
-	document.querySelector('#mobilemenu').classList.remove('mobile-menu');
-  });
+document.querySelector('#button3').addEventListener('click', () => {
+  document.querySelector('#mobilemenu').classList.add('mobile-menu-inactive');
+  document.querySelector('#mobilemenu').classList.remove('mobile-menu');
+});
 	
 });
