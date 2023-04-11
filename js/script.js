015503fb539b579f64f25c86935c5f3990763903
misalazeem <55094882+misalazeem@users.noreply.github.com>
2023-04-12 04:16:07 +0500

    fixing linter errors

--- js/script.js
+++ js/script.js
@@ -69,32 +69,31 @@ document.addEventListener('DOMContentLoaded', () => {
   ];
   const worksgrid = document.getElementById('works-grid');
   let popupblock = '<div id="container-exist" class="project-container"><div class="project-text-section"><div class="popup-heading"><h2 class="works-heading">' + works[i].projectname + '</h2><a href="#" class="popup-cancel" id="closeproject"><img src="./images/icons/desktop-popup-cancel.png"></a></div><div class="works-frame"><label class="frame-text1">Canopy</label><div class="frame-counter"><img src="images/Counter.png"></div><label class="frame-text2">Back End Dev</label><div class="frame-counter"><img src="images/Counter.png"></div><label class="frame-text2">2015</label></div><div class="img-container"><img src="' + works[i].featuredimage + '"></div><div class="img-container-desktop-popup"><img class="desktop-image" src="' + works[i].featuredimagedesktop + '"></div></div><div class="popup-desktop-format"><p class="works-paragraph">' + works[i].detaileddescription + '</p><div class="popup-desktop-format-2"><ul class="works-list"><li><label class="list-buttons">' + works[i].technologies[0] + '</label></li><li><label class="list-buttons">' + works[i].technologies[1] + '</label></li><li><label class="list-buttons">' + works[i].technologies[2] + '</label></li></ul><div class="popup-divider"></div><div class="project-button-section"><a href=' + works[i].linklive + ' class="works-button-1">See Live <img src="./images/icons/See-livve-icon.png"></img><a href="' + works[i].linksource + '"class="works-button-1">See Source <img src="./images/icons/github-button.png"></img></div></div></div><div class="navigation-links"><a class="popup-links" id="previous" href="#">Previous Project</a><a class="popup-links" id="next" href="#">Next Project</a></div></div>';
-  let worksgrid1;
-  worksgrid1 = document.getElementById('works-grid');
+  const worksgrid1 = document.getElementById('works-grid');
 
-  for (let j = 0; j < works.length; j = j + 1) {
-    var codeblock = '<div id="works-1">' + '<div class="img-container"><img src="' + works[j].featuredimage + '"></div>' + '<div class="img-container-desktop"><img class="desktop-image" src="' + works[j].featuredimagedesktop + '"></div>' + '<div class="works-contents">' + '<h2 class="works-heading">' + works[j].projectname + '</h2>' + '<div class="works-frame">' + '<label class="frame-text1">Canopy</label>' + '<div class="frame-counter"><img src="images/Counter.png"></div><label class="frame-text2">Back End Dev</label><div class="frame-counter"><img src="images/Counter.png"></div><label class="frame-text2">2015</label></div><p class="works-paragraph">' + works[j].projectdescription + '</p><ul class="works-list"><li><label class="list-buttons">' + works[j].technologies[0] + '</label></li><li><label class="list-buttons">' + works[j].technologies[1] + '</label></li><li><label class="list-buttons">' + works[j].technologies[2] + '</label></li></ul><button id="project' + j + '"class="works-button">See Project</button></div></div>';
+  for (let j = 0; j < works.length; j += 1) {
+    let codeblock = '<div id="works-1"> <div class="img-container"><img src="' + works[j].featuredimage + '"></div><div class="img-container-desktop"><img class="desktop-image" src="' + works[j].featuredimagedesktop + '"></div> <div class="works-contents"> <h2 class="works-heading">' + works[j].projectname + '</h2> <div class="works-frame"> <label class="frame-text1">Canopy</label> <div class="frame-counter"><img src="images/Counter.png"></div><label class="frame-text2">Back End Dev</label><div class="frame-counter"><img src="images/Counter.png"></div><label class="frame-text2">2015</label></div><p class="works-paragraph">' + works[j].projectdescription + '</p><ul class="works-list"><li><label class="list-buttons">' + works[j].technologies[0] + '</label></li><li><label class="list-buttons">' + works[j].technologies[1] + '</label></li><li><label class="list-buttons">' + works[j].technologies[2] + '</label></li></ul><button id="project' + j + '"class="works-button">See Project</button></div></div>';
     worksgrid.innerHTML += codeblock;
   }
-  
-  for (let j = 0 ; j < works.length; j = j + 1) {
+
+  for (let j = 0; j < works.length; j += 1) {
     popupblock = '<div id="project-popup' + j + '" class="project-popup-inactive"><div id="container-exist" class="project-container"><div class="project-text-section"><div class="popup-heading"><h2 class="works-heading">' + works[j].projectname + '</h2><a href="#" class="popup-cancel" id="closeproject' + j + '"><img src="./images/icons/desktop-popup-cancel.png"></a></div><div class="works-frame"><label class="frame-text1">Canopy</label><div class="frame-counter"><img src="images/Counter.png"></div><label class="frame-text2">Back End Dev</label><div class="frame-counter"><img src="images/Counter.png"></div><label class="frame-text2">2015</label></div><div class="img-container"><img src="' + works[j].featuredimage + '"></div><div class="img-container-desktop-popup"><img class="desktop-image" src="' + works[j].featuredimagedesktop + '"></div></div><div class="popup-desktop-format"><p class="works-paragraph">' + works[j].detaileddescription + '</p><div class="popup-desktop-format-2"><ul class="works-list"><li><label class="list-buttons">' + works[j].technologies[0] + '</label></li><li><label class="list-buttons">' + works[j].technologies[1] + '</label></li><li><label class="list-buttons">' + works[j].technologies[2] + '</label></li></ul><div class="popup-divider"></div><div class="project-button-section"><a href=' + works[j].linklive + 'class="works-button-1">See Live <img src="./images/icons/See-livve-icon.png"></img><a href="' + works[j].linksource + '"class="works-button-1">See Source <img src="./images/icons/github-button.png"></img></div></div></div><div class="navigation-links"><a class="popup-links" id="previous' + j + '" href="#">Previous Project</a><a class="popup-links" id="next' + j + '" href="#">Next Project</a></div></div></div>';
     worksgrid1.innerHTML += popupblock;
   }
-  var defaultnext = '#next' + (works.length - 1);
+  let defaultnext = '#next' + (works.length - 1);
   document.querySelector(defaultnext).classList.add('popup-links-disabled');
   document.querySelector(defaultnext).classList.remove('popup-links');
   document.querySelector('#previous0').classList.add('popup-links-disabled');
   document.querySelector('#previous0').classList.remove('popup-links');
   let index = 0;
-  for (let j = 0 ; j < works.length; j = j + 1) {
+  for (let j = 0; j < works.length; j += 1) {
     const projectname = '#project' + j;
     let popupname = '#project-popup' + j;
     let closebutton = '#closeproject' + j;
     const nextbutton = '#next' + j;
     const previousbutton = '#previous' + j;
     document.querySelector(projectname).addEventListener('click', () => {
-      popupname = '#project-popup'+projectname[8];
+      popupname = '#project-popup' + projectname[8];
       document.querySelector(popupname).classList.add('project-popup');
       document.querySelector(popupname).classList.remove('project-popup-inactive');
       index = projectname[8];
