12645e00fbe2fd6c5f60923aea64ab31da9fe9c6
misalazeem <55094882+misalazeem@users.noreply.github.com>
2023-09-19 23:28:14 +0500

    adding project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,11 +10,24 @@ import TodolistImage from '../images/Todolist.jpg';
 import VibetuneImage from '../images/vibetune.jpg';
 import AACPImage from '../images/AACP.jpg';
 import HNIImage from '../images/H&I.jpg';
+import SWimage from '../images/spendwise.jpg';
 
 function Projects() {
   const Projects = [
     {
       id: 1,
+      name: 'SpendWise',
+      stack: ['Ruby on Rails'],
+      summary: 'This is a Budget app built on ruby on rails. This is a full stack app which features login/signup of users who can add categories and transactions to track their spendings.',
+      image: SWimage,
+      year: '2023',
+      type: 'Full Stack Dev',
+      github: 'https://github.com/misalazeem/Budget-App',
+      livedemo: 'https://spendwise-sqra.onrender.com/',
+    },
+
+    {
+      id: 2,
       name: 'TVDB',
       stack: ['React', 'Redux', 'CSS3'],
       summary: 'This is a Tv show data base built on react. It retrieves data from API and renders it on the webpage. The data is stored inside the redux store. There is also a details page for each show which has further details of the TV show.',
@@ -25,7 +38,7 @@ function Projects() {
       livedemo: 'https://project-implementation--reliable-syrniki-0932c4.netlify.app/',
     },
     {
-      id: 2,
+      id: 3,
       name: 'MealDB',
       stack: ['HTML5', 'CSS3', 'Javascript ES6'],
       summary: 'This is a Meal DataBase built on Javascript ES6. It retrieves data from API and renders it on the webpage. User have abilities to expand each meal and browse the recipe. Also they can like an item and comment on each meal.',
@@ -36,7 +49,7 @@ function Projects() {
       livedemo: 'https://misalazeem.github.io/JavaScript-Capstone/',
     },
     {
-      id: 3,
+      id: 4,
       name: 'Space Travelers Hub',
       stack: ['React', 'Redux', 'CSS3'],
       summary: 'This is a React/Redux web application. It fetches data from the spaceX api to display rockets and missions. Users are able to join these missions and reserve the rockets.',
@@ -47,7 +60,7 @@ function Projects() {
       livedemo: 'https://dev--adorable-pegasus-239e60.netlify.app/',
     },
     {
-      id: 4,
+      id: 5,
       name: 'Bookstore',
       stack: ['React', 'Redux', 'CSS3'],
       summary: 'This is a BookStore CMS app built on React/Redux. You can add, remove books as functionalities in this project. The design is responsive and can be used as a Book Database.',
@@ -58,7 +71,7 @@ function Projects() {
       livedemo: 'https://dev--prismatic-druid-21b59f.netlify.app/',
     },
     {
-      id: 5,
+      id: 6,
       name: 'Math Magicians',
       stack: ['React', 'Redux', 'CSS3'],
       summary: 'This is a Math Magicians app. It is basically a calculator deisgned using react, There is also a Quote section which retrieves a quote from an API and displays it on the web page.',
@@ -69,7 +82,7 @@ function Projects() {
       livedemo: 'https://dev--chimerical-gecko-6642ce.netlify.app/',
     },
     {
-      id: 6,
+      id: 7,
       name: 'To Do List',
       stack: ['HTML5', 'CSS3', 'Javascript'],
       summary: 'This is a simple to do list project designed on JAVASCRIPT, HTML5 and CSS. It allow users to add daily tasks and remove them as they are completed.',
@@ -80,7 +93,7 @@ function Projects() {
       livedemo: 'https://misalazeem.github.io/To-Do-List/',
     },
     {
-      id: 7,
+      id: 8,
       name: 'Vibetune ',
       stack: ['HTML5', 'CSS3', 'Javascript'],
       summary: 'This is a Concert Event website built with HTML/CSS /JAVASCRIPT. Main features are responsive design, CSS flex boxes and grids.',
@@ -91,7 +104,7 @@ function Projects() {
       livedemo: 'https://misalazeem.github.io/Capstone-Project/',
     },
     {
-      id: 8,
+      id: 9,
       name: 'AACP',
       stack: ['WordPress'],
       summary: 'AACP is an E-Book and paid newsletter website. The website features an ecommerce store with working payment methods and memberships for its users.',
@@ -102,7 +115,7 @@ function Projects() {
       livedemo: 'https://aacp.com.pk/',
     },
     {
-      id: 9,
+      id: 10,
       name: 'Huzaima & Ikram',
       stack: ['WordPress'],
       summary: 'Huzaima & Ikram is a very popular Income-tax law firm based in Lahore, Pakistan. I have developed this website for them on Wordpress. It is mostly a portfolio but also features memberships for tax articles with a fully functioning e-commerce store.',
